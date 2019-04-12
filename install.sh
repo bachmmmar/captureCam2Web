d7ffb212fbd3d6249e60e633056b8448c209d1f4
Martin Bachmann <bachmmar@gmail.com>
2019-04-12 22:16:08 +0100

    !: obvious errors in installation script fixed

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+source scripts/checkSuccess.sh
+
 # install tools
-sudo apt-get install git-core tig tmux gphoto2 emacs-nox apache2 php5 libapache2-mod-php5 imagemagick
+sudo apt-get install gphoto2 apache2 php libapache2-mod-php imagemagick
+checkSuccess $? "installing packages"
 
 # change rights to access gpio and install python lib
 sudo usermod -a -G gpio pi
@@ -10,18 +13,21 @@ sudo usermod -a -G gpio pi
 #sudo chown root:gpio /dev/mem
 #sudo chmod g+wr /dev/mem
 sudo apt-get install python3-rpi.gpio
+checkSuccess $? "install python gpio"
 
-# enable python cgi for apache
 sudo a2enmod cgi
+checkSuccess $? "enable python cgi for apache"
 
 
 sudo chown www-data:users -R /var/www/html
 sudo mkdir /var/www/html/img
-sudo chmod g+s
+sudo chmod g+s /var/www/html/img
 
 sudo mkdir -p /opt/images/
 sudo chown pi:users -R /opt/images
+sudo chmod g+s /opt/images
 
+sudo rm /var/www/html/index.html
 sudo ln -s $(pwd)/www/index.html /var/www/html/index.html
 sudo ln -s $(pwd)/www/latest_pictures.php /var/www/html/latest_pictures.php
 sudo ln -s $(pwd)/www/filecontrol.py /var/www/html/filecontrol.py
